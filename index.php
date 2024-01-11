fa9eee3f4ac58f7a258f755760ddb1410132598a
Yul Caipang <ycaipang@gmail.com>
2024-01-12 06:26:32 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main Template file.
+ * 
+ * @package Aquila
+ */
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress</title>
+ </head>
+ <body>
+    Hello Wordpress
+ </body>
+ </html>
